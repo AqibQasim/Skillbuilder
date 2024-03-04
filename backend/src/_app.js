07af88a1f5bbe765a4ed0d0bc282c373468e72f7
Muhammad  Shahab <captain.shahab2002@gmail.com>
2024-03-04 13:30:23 +0500

    update server file

--- backend/src/_app.js
+++ backend/src/_app.js
@@ -5,8 +5,6 @@ const dataSource = require("../Infrastructure/postgres");
 const { logger } = require("../logger");
 const { fastifyOptions } = require("../fastifyOpts");
 const userRoutes = require("./routes/userRoutes");
-const {client} = require("../Infrastructure/redis")
-// const Validation=require('./Schema/userSchema')
 
 const startServer = async () => {
   const app = fastify(fastifyOptions);
@@ -21,20 +19,21 @@ const startServer = async () => {
   });
 
   app.register(userRoutes);
-  // app.register(Validation)
 
   try {
-    await app.listen(process.env.SERVER_PORT || 4000);
 
     await dataSource
-    .initialize()
-    .then((conn) => {
-      logger.info("Database connection has beed established ...");
+      .initialize()
+      .then(async (conn) => {
+        logger.info("Database connection has beed established ...");
+        await app.listen(process.env.SERVER_PORT, '0.0.0.0', () => {
+          logger.info(`Server is Listening on port ${process.env.SERVER_PORT} and environment is ${process.env.NODE_ENV}`);
+        });
       })
       .catch((error) => {
         logger.error(error);
       });
-    logger.info(`Server is Listening on ${process.env.NODE_ENV}`);
+
   } catch (error) {
     logger.error(error.message);
     process.exit(1);
