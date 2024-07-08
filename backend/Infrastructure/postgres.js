a29ad51a03ac70af79e178995998aae254f8c822
AqibQasim <muhammad44aqib@gmail.com>
2024-07-08 15:20:13 +0500

    dev

--- backend/Infrastructure/postgres.js
+++ backend/Infrastructure/postgres.js
@@ -6,13 +6,13 @@ const { PinoLogger, logger } = require("../logger");
 
 const dataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "18.119.213.12",
   port: process.env.DB_PORT || 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
   synchronize: true,
-  logging: true,  
+  logging: true,
   logger: new PinoLogger(),
   entities: [path.join(__dirname, "../src/entities/**/*.js")],
 });
