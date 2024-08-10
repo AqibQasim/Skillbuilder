d7c341f655afe5a9c01f798f84b60ea3fadf9c87
Taha <taha.akber007@gmail.com>
2024-08-10 14:33:06 +0500

    fixed reasons not updating in course table bug and get all instructors bug

--- backend/src/routes/instructorRoutes.js
+++ backend/src/routes/instructorRoutes.js
@@ -5,20 +5,27 @@ const {
   getCoursesByInstructor,
   uploadInstVideo,
   stripeAccRegister,
-  checkPaymentRecord
+  checkPaymentRecord,
 } = require("../controllers/InstructorController");
 
-const {createInstructorSchema, getAllInstructorsSchema, instructorDetailSchema} = require("../Schema/instructorSchema")
+const {
+  createInstructorSchema,
+  getAllInstructorsSchema,
+  instructorDetailSchema,
+} = require("../Schema/instructorSchema");
 
 const instructorRoutes = async (fastify, options) => {
+  fastify.post("/create-instructor", createInstructorSchema, createInstructor);
 
-  fastify.post("/create-instructor",createInstructorSchema, createInstructor);
-  
-  fastify.get("/get-all-instructors",getAllInstructorsSchema, getAllInstructor);
+  fastify.get("/get-all-instructors", getAllInstructor);
 
-  fastify.get("/instructor-detail/:id",instructorDetailSchema, instructorDetail);
+  fastify.get(
+    "/instructor-detail/:id",
+    instructorDetailSchema,
+    instructorDetail
+  );
 
-  fastify.get('/get-courses-inst/:id',getCoursesByInstructor)
+  fastify.get("/get-courses-inst/:id", getCoursesByInstructor);
 
   fastify.post("/upload", uploadInstVideo);
 
