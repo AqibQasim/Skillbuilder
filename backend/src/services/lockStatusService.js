8730248b4f04110ac47bbdc45b95c4195a0e3a04
Aahil Alwani <alwani.aahil25@gmail.com>
2024-11-25 21:05:36 +0500

    lock status

--- backend/src/services/lockStatusService.js
+++ backend/src/services/lockStatusService.js
@@ -1,45 +1,21 @@
 const dataSource = require("../../Infrastructure/postgres");
 const purchasedCourseRepo = dataSource.getRepository("purchased_course");
 
-const getLockStatusService = async (user_id, course_id, is_admin) => {
-  console.log(user_id, course_id, is_admin);
+const getLockStatusService = async (user_id, course_id) => {
   try {
-
-    if(is_admin=="true"){
-      return { 
-        status: 200, 
-        lock_status: "unlocked", 
-        lock: false 
-      }
+    const isPurchased = await purchasedCourseRepo.findOne({
+      where: {
+        course_id,
+        purchased_by: user_id,
+      },
+    });
+    if (!isPurchased) {
+      return { status: 403, lock_status: "locked", lock: true };
     }
-    // Check purchase only if the user is not admin
-    if (user_id!=null && is_admin == "false") {
-      const is_purchased = await purchasedCourseRepo.findOne({
-        where: {
-          course_id,
-          purchased_by: user_id,
-        },
-      });
-      console.log(is_purchased)
-      if (is_purchased != null) {
-        return {
-          status: 200,
-          lock_status: "unlocked",
-          lock: false,
-        };
-      }else{
-        return { 
-          status: 403, 
-          lock_status: "locked", 
-          lock: true 
-        }
-      }
-    }
-
-    return { 
-      status: 403, 
-      lock_status: "locked", 
-      lock: true 
+    return {
+      status: 200,
+      lock_status: "unlocked",
+      lock: false,
     };
   } catch (e) {
     return {
@@ -48,8 +24,6 @@ const getLockStatusService = async (user_id, course_id, is_admin) => {
     };
   }
 };
-
-
 module.exports = {
   getLockStatusService,
 };
