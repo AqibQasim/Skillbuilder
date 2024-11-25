cf98a246e56b2fd9efcf8954bea161207d0d7dab
Aahil Alwani <alwani.aahil25@gmail.com>
2024-11-25 20:34:56 +0500

    lock status bug fix

--- backend/src/services/lockStatusService.js
+++ backend/src/services/lockStatusService.js
@@ -5,27 +5,37 @@ const getLockStatusService = async (user_id, course_id, is_admin) => {
   console.log(user_id, course_id, is_admin);
   try {
 
+    if(is_admin=="true"){
+      return { 
+        status: 200, 
+        lock_status: "unlocked", 
+        lock: false 
+      }
+    }
     // Check purchase only if the user is not admin
-    if (user_id && is_admin == false) {
+    if (user_id!=null && is_admin == "false") {
       const is_purchased = await purchasedCourseRepo.findOne({
         where: {
-          course_id: parseInt(course_id),
-          purchased_by: parseInt(user_id),
+          course_id,
+          purchased_by: user_id,
         },
       });
       console.log(is_purchased)
-      if (is_purchased != null || is_admin == true) {
+      if (is_purchased != null) {
         return {
           status: 200,
           lock_status: "unlocked",
           lock: false,
         };
+      }else{
+        return { 
+          status: 403, 
+          lock_status: "locked", 
+          lock: true 
+        }
       }
     }
 
-    // Strictly check if purchased or if the user is admin
-
-    // Default to locked if no purchase and not admin
     return { 
       status: 403, 
       lock_status: "locked", 
