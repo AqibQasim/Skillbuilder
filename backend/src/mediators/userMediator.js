26caa79876e75c9a9cf9395f8c5d2f5bef56d50d
Aahil Alwani <alwani.aahil25@gmail.com>
2024-11-28 19:24:04 +0500

    add v1 in email verification

--- backend/src/mediators/userMediator.js
+++ backend/src/mediators/userMediator.js
@@ -30,7 +30,7 @@ const sendVerificationEmail = async (email, verificationToken) => {
       html: `
       <h1>Click on the link below to verify your email!</h1>
       
-      <a href="${process.env.SERVER_BBASE_URL}/verify-email?email=${encodeURIComponent(
+      <a href="${process.env.SERVER_BASE_URL}/verify-email?email=${encodeURIComponent(
           email
         )}&token=${verificationToken}">Verify your email</a>`,
     };
