71da3fb5bea5c68b74d36dc5fe76ca3db557870e
Aahil Alwani <alwani.aahil25@gmail.com>
2024-11-28 18:53:30 +0500

    add v1, fix create-course api

--- backend/src/repositories/courseRepository.js
+++ backend/src/repositories/courseRepository.js
@@ -4,31 +4,16 @@ const courseContent = require("../entities/courseContent");
 const courseRepository = dataSource.getRepository("Course");
 const courseRevRep = dataSource.getRepository("courseReviews");
 const courseContentRepository = dataSource.getRepository("course_content");
-const uuid = require("uuid");
+const uuid= require('uuid');
 const { base64_decode } = require("../utils/base64_decode");
 const { join } = require("lodash");
 
 const createCourse = async (data) => {
   try {
-    // Generate a random file name with the appropriate extension
-    const randomFileName = uuid.v4() + "." + data.image.extension;
-
-    // Define the target directory
-    const targetDir = join(process.cwd(), "media", "images", "course");
-
-    // Ensure the Base64 string does not include the prefix
-    const base64String = data.image.image.replace(
-      /^data:image\/\w+;base64,/,
-      ""
-    );
-
-    // Call the function
-    base64_decode(base64String, randomFileName, targetDir);
-
-    const courseCreating = courseRepository.create({
-      ...data,
-      image: randomFileName.toString(),
-    });
+    const randomFileName= uuid.v4()+'.'+data.image.extension;
+    const targetDir= join(process.cwd(),"media","images","course");
+    base64_decode(data.image.image, randomFileName,targetDir);
+    const courseCreating = courseRepository.create({...data, image: randomFileName.toString()});
     const courseBasics = await courseRepository.save(courseCreating);
     return courseBasics;
   } catch (error) {
@@ -365,7 +350,7 @@ const studentEnrolledCoursesOnInstructorRepository = async (
           learning_outcomes: course.learning_outcomes,
           amount: course.amount,
           rating: course.rating,
-          id: course.id,
+          id: course.id
         });
       }
     });
@@ -392,6 +377,6 @@ module.exports = {
   setCourseStatusRepository,
   findOneCourseWithStudentID,
   studentEnrolledCoursesOnInstructorRepository,
-  findAllStudentCourses,
+  findAllStudentCourses
   // saveReview
 };
