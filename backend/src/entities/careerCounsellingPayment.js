a4c13ea4c930a15673b2b0382db65ebda2273106
Sanjay Kumar <sanjay1402k@gmail.com>
2025-01-15 20:29:30 +0500

    booking nullable

--- backend/src/entities/careerCounsellingPayment.js
+++ backend/src/entities/careerCounsellingPayment.js
@@ -1,55 +1,57 @@
 const { EntitySchema } = require("typeorm");
 
 module.exports = new EntitySchema({
-    target: "CareerCounsellingPayments",
-    name: "career-counselling-payments",
-    tableName: "CareerCounsellingPayments",
-    columns: {
-        id: {
-            primary: true,
-            type: "int",
-            generated: true,
-        },
-        student_id: {
-            type: "int",
-        },
-        instructor_id: {
-            type: "int",
-            nullable: true,
-        },
-        amount: {
-            type: "int",
-            default: 25
-        },
-        booking_date:{
-            type: "varchar",
-        },
-        booking_time:{
-            type: "varchar"
-        },
-        created_at: {
-            type: "timestamp",
-            default: () => "CURRENT_TIMESTAMP",
-        },
+  target: "CareerCounsellingPayments",
+  name: "career-counselling-payments",
+  tableName: "CareerCounsellingPayments",
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: true,
     },
-    relations: {
-        student: {
-            target: "User",
-            type: "one-to-one",
-            //inverseSide: "carts",
-            joinColumn: {
-                name: "student_id",
-                referencedColumnName: "id",
-            },
-        },
-        instructor: {
-            target: "Instructor",
-            type: "one-to-one",
-            //inverseSide: "carts",
-            joinColumn: {
-                name: "instructor_id",
-                //referencedColumnName: "id",
-            },
-        },
+    student_id: {
+      type: "int",
     },
+    instructor_id: {
+      type: "int",
+      nullable: true,
+    },
+    amount: {
+      type: "int",
+      default: 25,
+    },
+    booking_date: {
+      type: "varchar",
+      nullable: true,
+    },
+    booking_time: {
+      type: "varchar",
+      nullable: true,
+    },
+    created_at: {
+      type: "timestamp",
+      default: () => "CURRENT_TIMESTAMP",
+    },
+  },
+  relations: {
+    student: {
+      target: "User",
+      type: "one-to-one",
+      //inverseSide: "carts",
+      joinColumn: {
+        name: "student_id",
+        referencedColumnName: "id",
+      },
+    },
+    instructor: {
+      target: "Instructor",
+      type: "one-to-one",
+      //inverseSide: "carts",
+      joinColumn: {
+        name: "instructor_id",
+        //referencedColumnName: "id",
+      },
+    },
+  },
 });
