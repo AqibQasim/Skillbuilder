6fa2bce0c0b48785a76fdf6cec01c20da59a228f
Aahil Alwani <alwani.aahil25@gmail.com>
2025-01-22 17:16:37 +0500

    feat: courses filter by topic

--- backend/src/controllers/searchController.js
+++ backend/src/controllers/searchController.js
@@ -2,9 +2,15 @@ const searchCourse = require("../services/searchService");
 
 const search= async(req,res)=>{
     const {value,searchBy}= req?.query;
+    let result=null;
     switch(searchBy){
         case 'course':
-            const result= await searchCourse(value);
+            result= await searchCourse.searchCourse(value);
+            res.status(result.status).send({...result});
+            break;
+        case 'topic':
+            console.log(value)
+            result= await searchCourse.searchCourses(value);
             res.status(result.status).send({...result});
             break;
         default:
--- backend/src/services/searchService.js
+++ backend/src/services/searchService.js
@@ -30,4 +30,46 @@ const searchCourse= async(courseName)=>{
     };
 }
 
-module.exports= searchCourse;
+const searchCourses= async(courseNames)=>{
+    if (!courseNames?.length===0) {
+        return null;
+    }
+
+    const courseArray= courseNames?.split(/\[|\]|\,/);
+    const coursearrayLength= courseArray?.length-2;
+    const parsedCourseArray= courseArray?.splice(1,coursearrayLength);
+    
+    console.log(parsedCourseArray)
+
+    let courseResults=[]
+    for(let courseName of parsedCourseArray){
+        
+        const courses = await courseRepository
+            .createQueryBuilder("course")
+            .innerJoinAndSelect("course.instructor","instructor")
+            .innerJoinAndSelect("instructor.user","user","instructor.user_id=user.id")
+            .where("course.title ILIKE :courseName", { courseName: `%${courseName?.toString()}%` })
+            .orWhere("course.learning_outcomes ILIKE :courseName",{courseName:`%${courseName?.toString()}%`})
+            .andWhere("course.status='approved'")
+            .getMany();
+        console.log(courses)
+        if(courses?.length>0){
+            courseResults=[...courseResults,...courses];
+        }
+    }
+
+    if(courseResults?.length===0){
+        return {
+            status: 404,
+            message:"topics not found",
+        }
+    }
+
+    return {
+        status: 200,
+        message: "topics found",
+        data: courseResults
+    };
+}
+
+module.exports= {searchCourse,searchCourses};
