2c5ec3dde7dfb8b2c22aca99d52c9159a8fd4f60
Aahil Alwani <alwani.aahil25@gmail.com>
2025-01-23 20:43:11 +0500

    chore: get the specific email

--- backend/src/services/paymentService.js
+++ backend/src/services/paymentService.js
@@ -105,7 +105,8 @@ const getCareerCounsellingPayment = async (req) => {
         "career_counselling_payments.recruitinn_summary",
         "career_counselling_payments.created_at",
         "student.first_name",
-        "student.last_name"
+        "student.last_name",
+        "student.email"
       ])
       .where("career_counselling_payments.student_id= :student_id",{student_id})
       .getOne();
