833c007eb3a91c948d335a8f53205160a767bcf3
Aahil Alwani <alwani.aahil25@gmail.com>
2025-02-07 12:18:52 +0500

    fix: 500 error due to the change datatype of learning outcomes

--- backend/src/services/searchService.js
+++ backend/src/services/searchService.js
@@ -2,74 +2,86 @@ const dataSource = require("../../Infrastructure/postgres");
 
 const courseRepository = dataSource.getRepository("Course");
 
-const searchCourse= async(courseName)=>{
-    if (!courseName) {
-        return null;
-    }
+const searchCourse = async (courseName) => {
+  if (!courseName) {
+    return null;
+  }
 
+  const courses = await courseRepository
+    .createQueryBuilder("course")
+    .innerJoinAndSelect("course.instructor", "instructor")
+    .innerJoinAndSelect("instructor.user", "user", "instructor.user_id=user.id")
+    .where("course.title ILIKE :courseName", { courseName: `%${courseName}%` })
+    .orWhere("course.learning_outcomes::text ILIKE :courseName", {
+      courseName: `%${courseName}%`,
+    })
+    .orWhere("course.description ILIKE :courseName", {
+      courseName: `%${courseName}%`,
+    })
+    .andWhere("course.status='approved'")
+    .getMany();
+
+  if (courses?.length === 0) {
+    return {
+      status: 404,
+      message: "course not found",
+    };
+  }
+
+  return {
+    status: 200,
+    message: "course found",
+    data: courses[0],
+  };
+};
+
+const searchCourses = async (courseNames) => {
+  if (!courseNames?.length === 0) {
+    return null;
+  }
+
+  const courseArray = courseNames?.split(/\[|\]|\,/);
+  const coursearrayLength = courseArray?.length - 2;
+  const parsedCourseArray = courseArray?.splice(1, coursearrayLength);
+
+  console.log(parsedCourseArray);
+
+  let courseResults = [];
+  for (let courseName of parsedCourseArray) {
     const courses = await courseRepository
-        .createQueryBuilder("course")
-        .innerJoinAndSelect("course.instructor","instructor")
-        .innerJoinAndSelect("instructor.user","user","instructor.user_id=user.id")
-        .where("course.title ILIKE :courseName", { courseName: `%${courseName}%` })
-        .orWhere("course.learning_outcomes ILIKE :courseName",{courseName:`%${courseName}%`})
-        .andWhere("course.status='approved'")
-        .getMany();
-
-    if(courses?.length===0){
-        return {
-            status: 404,
-            message:"course not found",
-        }
+      .createQueryBuilder("course")
+      .innerJoinAndSelect("course.instructor", "instructor")
+      .innerJoinAndSelect(
+        "instructor.user",
+        "user",
+        "instructor.user_id=user.id"
+      )
+      .where("course.title ILIKE :courseName", {
+        courseName: `%${courseName?.toString()}%`,
+      })
+      .orWhere("course.learning_outcomes ILIKE :courseName", {
+        courseName: `%${courseName?.toString()}%`,
+      })
+      .andWhere("course.status='approved'")
+      .getMany();
+    console.log(courses);
+    if (courses?.length > 0) {
+      courseResults = [...courseResults, ...courses];
     }
+  }
 
+  if (courseResults?.length === 0) {
     return {
-        status: 200,
-        message: "course found",
-        data: courses[0]
+      status: 404,
+      message: "topics not found",
     };
-}
+  }
 
-const searchCourses= async(courseNames)=>{
-    if (!courseNames?.length===0) {
-        return null;
-    }
+  return {
+    status: 200,
+    message: "topics found",
+    data: courseResults,
+  };
+};
 
-    const courseArray= courseNames?.split(/\[|\]|\,/);
-    const coursearrayLength= courseArray?.length-2;
-    const parsedCourseArray= courseArray?.splice(1,coursearrayLength);
-    
-    console.log(parsedCourseArray)
-
-    let courseResults=[]
-    for(let courseName of parsedCourseArray){
-        
-        const courses = await courseRepository
-            .createQueryBuilder("course")
-            .innerJoinAndSelect("course.instructor","instructor")
-            .innerJoinAndSelect("instructor.user","user","instructor.user_id=user.id")
-            .where("course.title ILIKE :courseName", { courseName: `%${courseName?.toString()}%` })
-            .orWhere("course.learning_outcomes ILIKE :courseName",{courseName:`%${courseName?.toString()}%`})
-            .andWhere("course.status='approved'")
-            .getMany();
-        console.log(courses)
-        if(courses?.length>0){
-            courseResults=[...courseResults,...courses];
-        }
-    }
-
-    if(courseResults?.length===0){
-        return {
-            status: 404,
-            message:"topics not found",
-        }
-    }
-
-    return {
-        status: 200,
-        message: "topics found",
-        data: courseResults
-    };
-}
-
-module.exports= {searchCourse,searchCourses};
+module.exports = { searchCourse, searchCourses };
